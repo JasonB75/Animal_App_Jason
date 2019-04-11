a364370d6b94225665adfca6e82569ad4c4037f2
JasonB75 <37903292+JasonB75@users.noreply.github.com>
2019-04-11 10:24:01 -0400

    WALL

--- app/src/main/java/com/superking75/animal_app_jason/Fox.java
+++ app/src/main/java/com/superking75/animal_app_jason/Fox.java
@@ -3,7 +3,7 @@ package com.superking75.animal_app_jason;
 public class Fox extends Animal implements Fun
 {
 
-    public String[] foxSay = {"","Ring-ding-ding-ding-dingeringeding!","Wa-pa-pa-pa-pa-pa-pow!","Hatee-hatee-hatee-ho!", "Joff-tchoff-tchoffo-tchoffo-tchoff!", "Fraka-kaka-kaka-kaka-kow!","A-hee-ahee ha-hee!"};
+    public String[] foxSay = {"","Ring-ding-ding-ding-dingeringeding!","Wa-pa-pa-pa-pa-pa-pow!","Hatee-hatee-hatee-ho!", "Joff-tchoff-tchoffo-tchoffo-tchoff!", "Fraka-kaka-kaka-kaka-kow!","A-hee-ahee ha-hee!", "BUILD THE WAAAAAAAAAAAAALLLLLLL"};
     public Fox()
     {
         type = "Fox";
@@ -16,7 +16,7 @@ public class Fox extends Animal implements Fun
     @Override
     public String say()
     {
-        int random = (int ) (Math.random() * 6);
+        int random = (int ) (Math.random() * 7);
         return foxSay[random];
 
     }
